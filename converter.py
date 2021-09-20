441924f6a062d72c856dff89ddd92cb53ffa4661
Kai_Yoshida <14451@g.nagano-nct.ac.jp>
2021-09-21 00:35:00 +0900

    発話に改行コードが混ざってるとエラーが出てたので修正

--- converter.py
+++ converter.py
@@ -1,35 +1,31 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
+# In[113]:
 
 
 import pandas as pd
+from tqdm import tqdm
 
 
 # # Read Input xlsx files
 
-# In[9]:
+# In[114]:
 
 
 df_conversation = pd.read_excel("./data/japanese_persona_chat.xlsx", sheet_name="対話")
+df_conversation["発話"] = df_conversation["発話"].replace( '\n', '', regex=True)
 df_conversation
 
 
-# In[5]:
+# In[115]:
 
 
 df_persona = pd.read_excel("./data/japanese_persona_chat.xlsx", sheet_name="ペルソナリスト")
 df_persona
 
 
-# In[89]:
-
-
-df_user[0:1]["発話"].values[0]
-
-
-# In[112]:
+# In[116]:
 
 
 df_groupby = df_conversation.groupby(["No","ペルソナID"])
@@ -49,12 +45,12 @@ for i in range(1, df_conversation["No"].max()+1):
 df_outputA
 
 
-# In[117]:
+# In[ ]:
 
 
 df_groupby = df_conversation.groupby(["No","ペルソナID"])
 df_outputB = pd.DataFrame(columns=["ペルソナID","A1","B1","A2"])
-for i in range(1, df_conversation["No"].max()+1):
+for i in tqdm(range(1, df_conversation["No"].max()+1)):
     df_user = df_groupby.get_group((i,f"PP{i}"))
     for j in range(0,len(df_user)-3,2):
         df_outputB = df_outputB.append(
@@ -75,7 +71,7 @@ df_outputB
 
 
 with open("./j_perchat.txt", mode="w", encoding="utf-8") as f:
-    for pid in df_persona["ペルソナID"].unique().tolist():
+    for pid in tqdm(df_persona["ペルソナID"].unique().tolist()):
         i = 1
         # ペルソナの書き出し
         for persona in df_persona[df_persona["ペルソナID"]==pid]["A"].values[0].split("\n"):
@@ -83,7 +79,7 @@ with open("./j_perchat.txt", mode="w", encoding="utf-8") as f:
             i+=1
         # 対話ペアの書き出し
         for index, row in df_outputA[df_outputA["ペルソナID"]==pid].iterrows():
-            f.write(f'{i} {row["A1"]}\t{row["B1"]}\t{row["A2"]}\n')
+            f.write(f'{i} {row["A1"]}\t{row["B1"]}\t\t{row["A2"]}\n')
             i+=1
         i = 1
         # ペルソナの書き出し
@@ -92,7 +88,7 @@ with open("./j_perchat.txt", mode="w", encoding="utf-8") as f:
             i+=1
         # 対話ペアの書き出し
         for index, row in df_outputB[df_outputB["ペルソナID"]==pid].iterrows():
-            f.write(f'{i} {row["B1"]}\t{row["A1"]}\t{row["B2"]}\n')
+            f.write(f'{i} {row["B1"]}\t{row["A1"]}\t\t{row["B2"]}\n')
             i+=1         
     
 
